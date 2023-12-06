a814168ed5afa3eb11b1248c9ab862393c2a1b27
Asiiaaaa <anastasiya23091988@gmail.com>
2023-12-06 21:12:46 +0300

    add correct example006

--- Example006_Hello/Program.cs
+++ Example006_Hello/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Masha")
+{
+    Console.WriteLine("Ура, это же Masha");
+}
+else
+{
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+}
