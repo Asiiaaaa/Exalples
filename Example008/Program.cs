c7038df184afbe4c0228e7daf09a2f59cdb4049a
Asiiaaaa <anastasiya23091988@gmail.com>
2023-12-09 18:44:39 +0300

    add example008 and seminar_massiv

--- Example008/Program.cs
+++ Example008/Program.cs
@@ -1,12 +1,12 @@
 ﻿Console.Clear();
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
-int xa = 15;
+int xa = 1;
 int ya = 10;
-int xb = 1;
-int yb = 10;
-int xc = 30;
-int yc = 10;
+int xb = 20;
+int yb = 1;
+int xc = 20;
+int yc = 40;
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine ("*");
 
@@ -15,12 +15,14 @@ Console.WriteLine ("*");
 
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine ("*");
- int x = xa, y = xb;
+
+ int x = xa, y = ya;
+ 
  int count = 0;
 
 while(count<10)
 {
-    int what = new Random().Next(0, 10);
+    int what = new Random().Next(0, 3);
 
     if(what == 0)
     {
@@ -30,7 +32,7 @@ while(count<10)
     if(what == 1)
     {
         x = (x + xb)/2;
-        ya = (y + yb)/2;
+        y = (y + yb)/2;
     }
     if(what == 2)
 
